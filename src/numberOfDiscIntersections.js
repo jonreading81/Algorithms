7b44c3326e5ed7eb47db78c10a47082ff55f0cea
Jonathan Reading <jon.reading.email@gmail.com>
2017-06-22 14:42:14 +0100

    NumberOfDiscIntersections

---
+++ src/numberOfDiscIntersections.js
@@ -0,0 +1,59 @@
+
+const solution = function (A) {
+  let lowerLimits = [];
+  let upperLimits = [];
+  let intersections = 0;
+  let searchIntersections;
+  let i;
+  let j;
+  const compare = (a, b) => a - b;
+
+  A.forEach((radius, index) => {
+    lowerLimits.push(index - radius);
+    upperLimits.push(index + radius);
+  });
+  lowerLimits.sort(compare);
+  upperLimits.sort(compare);
+
+  for (i = 0 ; i < A.length; i++) {
+    j = 0;
+    searchIntersections = -1;
+    while (upperLimits[i] >= lowerLimits[j]) {
+      j++;
+      searchIntersections++;
+    }
+    intersections += searchIntersections - i;
+    if (intersections > 10000000) return -1;
+  }
+  return intersections;
+};
+/*
+const solutionSimple = function (A) {
+  let i;
+  let j;
+  let intersections = 0;
+  let circleA = {};
+  let circleB = {};
+
+  for(i = 0; i < A.length; i++) {
+    circleA = {
+      radius: A[i],
+      centre: i
+    };
+    for(j = i + 1; j < A.length; j++) {
+      circleB = {
+        radius: A[j],
+        centre: j
+      };
+      if (circleA.centre + circleA.radius >= circleB.centre - circleB.radius) {
+        intersections++;
+        if(intersections > 10000000) return -1;
+      }
+
+    }
+  }
+  return intersections;
+}
+*/
+
+export default solution;
---
+++ test/numberOfDiscIntersections.spec.js
@@ -0,0 +1,16 @@
+import solution from '../src/numberOfDiscIntersections';
+
+describe.only('NumberOfDiscIntersections', function () {
+  it('should solve codility example', function () {
+    expect(solution([1,5,2,1,4,0])).to.equal(11);
+  });
+
+  it('should solve simple 3 small circles', function () {
+    expect(solution([1,1,1])).to.equal(3);
+  });
+
+  it('should solve codility example', function () {
+    expect(solution([1,1,1,0,0,4])).to.equal(9);
+  });
+  
+});
