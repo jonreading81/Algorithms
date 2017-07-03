183d97c81b47ed8cb3d14767e4e41ac42d977b67
Jonathan Reading <jon.reading.email@gmail.com>
2017-07-03 13:26:00 +0100

    Choclate by numbers

---
+++ src/chocolateByNumbers.js
@@ -0,0 +1,22 @@
+const gcd = function (a, b) {
+  let result;
+
+  if (a % b === 0) {
+    result = b;
+  } else {
+    result = gcd(b, a % b);
+  }
+  return result;
+};
+
+const lcm = function (a, b) {
+  return (a * b) / gcd(a, b);
+};
+
+const solution = function (n, m) {
+  console.log(lcm(n, m));
+  return lcm(n, m) / m;
+
+};
+
+export default solution;
---
+++ test/chocolateByNumber.spec.js
@@ -0,0 +1,8 @@
+import solution from '../src/chocolateByNumbers';
+
+describe('chocolateByNumbers', function () {
+  it('should solve codility example', function () {
+    expect(solution(10, 4)).to.equal(5);
+
+  });
+});
